3bcea2ce2fae6c57d7b7bda93039024cbbe5cfd4
Yeser Amer <yeser.amer@ibm.com>
2022-10-12 10:17:57 +0200

    Example created

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Person {
+
+    private String firstName;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
